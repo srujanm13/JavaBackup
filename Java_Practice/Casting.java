d6af4e0eaa124f11f1e5001dd7b21cc3e28aa111
Srujan Kumar reddy Mayaluru <srujanm.in@mouritech.com>
2019-05-22 15:40:04 +0530

    adding work files

--- Java_Practice/Casting.java
+++
@@ -1,7 +0,0 @@
-class casting{
-public static void main (String args[]){
-float a=10.2f;
-int b=(int)a;
-System.out.println(b);
-}
-}
--- Java_Practice/PrintVariableValue.java
+++
@@ -1,8 +0,0 @@
-class PrintVariableValue{
-public static void main (String atgs[]){
-int no_of_stiudents=60;
-String highest_marks="98";
-System.out.println("In section A consisting of "+no_of_stiudents+"Srujan Got"+highest_marks+"Marks");
-
-}
-}
